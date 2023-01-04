c365ee2c5902a2ff236c8a4529489f4ad9c5b69a
la niina <la.niina.me@gmail.com>
2023-01-04 15:06:22 +0300

    int

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
+import React from "react";
 import { Inter } from '@next/font/google'
-import { Navbar, Button, Link, Text, useTheme, Avatar, Dropdown } from "@nextui-org/react";
+import { Navbar, Button, Link, Text, useTheme, Avatar, Dropdown, Modal, Input, Row, Checkbox } from "@nextui-org/react";
 import { Layout } from "../compose/Navbar/Layout.js";
 import { AcmeLogo } from "../compose/Navbar/AcmeLogo.js";
+import { Mail } from "../compose/LoginModel/Mail";
+import { Password } from "../compose/LoginModel/Password";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +24,12 @@ export default function Home() {
     "Help & Feedback",
     "Log Out",
   ];
-
+  const [visible, setVisible] = React.useState(false);
+  const handler = () => setVisible(true);
+  const closeHandler = () => {
+    setVisible(false);
+    console.log("closed");
+  };
   return (
     <>
       <Head>
@@ -30,109 +38,120 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <Layout>
-          <Navbar isBordered variant="sticky">
-            <Navbar.Toggle showIn="xs" />
-            <Navbar.Brand
-              css={{
-                "@xs": {
-                  w: "12%",
-                },
-              }}
-            >
-              <AcmeLogo />
-              <Text b color="inherit" hideIn="xs">
-                ACME
+      <Layout>
+        <Navbar isBordered variant="sticky">
+          <Navbar.Toggle showIn="xs" />
+          <Navbar.Brand
+            css={{
+              "@xs": {
+                w: "12%",
+              },
+            }}
+          >
+            <AcmeLogo />
+            <Text b color="inherit" hideIn="xs">
+              IIo
+            </Text>
+          </Navbar.Brand>
+
+          <Navbar.Content
+            enableCursorHighlight
+            activeColor="secondary"
+            hideIn="xs"
+            variant="highlight-rounded"
+          >
+            <Navbar.Link href="/">features</Navbar.Link>
+            <Navbar.Link isActive href="/customers">
+              customers
+            </Navbar.Link>
+            <Navbar.Link href="/concepts">concepts</Navbar.Link>
+            <Navbar.Link href="/about">about</Navbar.Link>
+          </Navbar.Content>
+
+          <Navbar.Content>
+            <Navbar.Item>
+              <Button auto flat as={Link} onClick={handler}>
+                Sign Up
+              </Button>
+            </Navbar.Item>
+          </Navbar.Content>
+
+          <Modal
+            closeButton
+            preventClose
+            aria-labelledby="modal-title"
+            open={visible}
+            onClose={closeHandler}
+          >
+            <Modal.Header>
+              <Text id="modal-title" size={60}
+                css={{
+                  textGradient: "45deg, $blue600 -20%, $pink600 50%",
+                }}
+                weight="bold">
+                IIo 
               </Text>
-            </Navbar.Brand>
-            <Navbar.Content
-              enableCursorHighlight
-              activeColor="secondary"
-              hideIn="xs"
-              variant="highlight-rounded"
-            >
-              <Navbar.Link href="#">features</Navbar.Link>
-              <Navbar.Link isActive href="#">
-                customers
-              </Navbar.Link>
-              <Navbar.Link href="#">concepts</Navbar.Link>
-              <Navbar.Link href="#">about</Navbar.Link>
-            </Navbar.Content>
-            <Navbar.Content
-              css={{
-                "@xs": {
-                  w: "12%",
-                  jc: "flex-end",
-                },
-              }}
-            >
-              <Dropdown placement="bottom-right">
-                <Navbar.Item>
-                  <Dropdown.Trigger>
-                    <Avatar
-                      bordered
-                      as="button"
-                      color="secondary"
-                      size="md"
-                      src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                    />
-                  </Dropdown.Trigger>
-                </Navbar.Item>
-                <Dropdown.Menu
-                  aria-label="User menu actions"
-                  color="secondary"
-                  onAction={(actionKey) => console.log({ actionKey })}
-                >
-                  <Dropdown.Item key="profile" css={{ height: "$18" }}>
-                    <Text b color="inherit" css={{ d: "flex" }}>
-                      Signed in as
-                    </Text>
-                    <Text b color="inherit" css={{ d: "flex" }}>
-                      zoey@example.com
-                    </Text>
-                  </Dropdown.Item>
-                  <Dropdown.Item key="settings" withDivider>
-                    My Settings
-                  </Dropdown.Item>
-                  <Dropdown.Item key="team_settings">Team Settings</Dropdown.Item>
-                  <Dropdown.Item key="analytics" withDivider>
-                    Analytics
-                  </Dropdown.Item>
-                  <Dropdown.Item key="system">System</Dropdown.Item>
-                  <Dropdown.Item key="configurations">Configurations</Dropdown.Item>
-                  <Dropdown.Item key="help_and_feedback" withDivider>
-                    Help & Feedback
-                  </Dropdown.Item>
-                  <Dropdown.Item key="logout" withDivider color="error">
-                    Log Out
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </Navbar.Content>
-            <Navbar.Collapse>
-              {collapseItems.map((item, index) => (
-                <Navbar.CollapseItem
-                  key={item}
-                  activeColor="secondary"
+            </Modal.Header>
+            <Modal.Body>
+              <Input
+                clearable
+                bordered
+                fullWidth
+                color="primary"
+                size="lg"
+                placeholder="Email"
+                contentLeft={<Mail fill="currentColor" />}
+              />
+              <Input
+                clearable
+                bordered
+                fullWidth
+                color="primary"
+                size="lg"
+                placeholder="Password"
+                contentLeft={<Password fill="currentColor" />}
+              />
+              <Row justify="space-between">
+                <Checkbox>
+                  <Text size={14}>Remember me</Text>
+                </Checkbox>
+                <Text size={14}>Forgot password?</Text>
+              </Row>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button auto flat color="error" onClick={closeHandler}>
+                Close
+              </Button>
+              <Button auto onClick={closeHandler}>
+                Sign in
+              </Button>
+            </Modal.Footer>
+          </Modal>
+
+          <Navbar.Collapse>
+            {collapseItems.map((item, index) => (
+              <Navbar.CollapseItem
+                key={item}
+                activeColor="secondary"
+                css={{
+                  color: index === collapseItems.length - 1 ? "$error" : "",
+                }}
+                isActive={index === 2}
+              >
+                <Link
+                  color="inherit"
                   css={{
-                    color: index === collapseItems.length - 1 ? "$error" : "",
+                    minWidth: "100%",
                   }}
-                  isActive={index === 2}
+                  href="#"
                 >
-                  <Link
-                    color="inherit"
-                    css={{
-                      minWidth: "100%",
-                    }}
-                    href="#"
-                  >
-                    {item}
-                  </Link>
-                </Navbar.CollapseItem>
-              ))}
-            </Navbar.Collapse>
-          </Navbar>
-        </Layout>
+                  {item}
+                </Link>
+              </Navbar.CollapseItem>
+            ))}
+          </Navbar.Collapse>
+        </Navbar>
+      </Layout>
     </>
   )
 }
