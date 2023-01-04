bf294be865976d72d5f138fccc68289b729df79c
la niina <la.niina.me@gmail.com>
2023-01-04 14:45:19 +0300

    int

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
+
+const theme = createTheme({
+  type: "dark", // it could be "light" or "dark"
+  theme: {
+    colors: {
+      primary: '#4ADE7B',
+      secondary: '#F9CB80',
+      error: '#FCC5D8',
+    },
+  }
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider theme={theme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import { Navbar, Button, Link, Text, useTheme, Avatar, Dropdown } from "@nextui-org/react";
+import { Layout } from "../compose/Navbar/Layout.js";
+import { AcmeLogo } from "../compose/Navbar/AcmeLogo.js";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { isDark } = useTheme();
+
+  const collapseItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+
   return (
     <>
       <Head>
@@ -14,110 +30,109 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
+        <Layout>
+          <Navbar isBordered variant="sticky">
+            <Navbar.Toggle showIn="xs" />
+            <Navbar.Brand
+              css={{
+                "@xs": {
+                  w: "12%",
+                },
+              }}
             >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+              <AcmeLogo />
+              <Text b color="inherit" hideIn="xs">
+                ACME
+              </Text>
+            </Navbar.Brand>
+            <Navbar.Content
+              enableCursorHighlight
+              activeColor="secondary"
+              hideIn="xs"
+              variant="highlight-rounded"
+            >
+              <Navbar.Link href="#">Features</Navbar.Link>
+              <Navbar.Link isActive href="#">
+                Customers
+              </Navbar.Link>
+              <Navbar.Link href="#">Pricing</Navbar.Link>
+              <Navbar.Link href="#">Company</Navbar.Link>
+            </Navbar.Content>
+            <Navbar.Content
+              css={{
+                "@xs": {
+                  w: "12%",
+                  jc: "flex-end",
+                },
+              }}
+            >
+              <Dropdown placement="bottom-right">
+                <Navbar.Item>
+                  <Dropdown.Trigger>
+                    <Avatar
+                      bordered
+                      as="button"
+                      color="secondary"
+                      size="md"
+                      src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                    />
+                  </Dropdown.Trigger>
+                </Navbar.Item>
+                <Dropdown.Menu
+                  aria-label="User menu actions"
+                  color="secondary"
+                  onAction={(actionKey) => console.log({ actionKey })}
+                >
+                  <Dropdown.Item key="profile" css={{ height: "$18" }}>
+                    <Text b color="inherit" css={{ d: "flex" }}>
+                      Signed in as
+                    </Text>
+                    <Text b color="inherit" css={{ d: "flex" }}>
+                      zoey@example.com
+                    </Text>
+                  </Dropdown.Item>
+                  <Dropdown.Item key="settings" withDivider>
+                    My Settings
+                  </Dropdown.Item>
+                  <Dropdown.Item key="team_settings">Team Settings</Dropdown.Item>
+                  <Dropdown.Item key="analytics" withDivider>
+                    Analytics
+                  </Dropdown.Item>
+                  <Dropdown.Item key="system">System</Dropdown.Item>
+                  <Dropdown.Item key="configurations">Configurations</Dropdown.Item>
+                  <Dropdown.Item key="help_and_feedback" withDivider>
+                    Help & Feedback
+                  </Dropdown.Item>
+                  <Dropdown.Item key="logout" withDivider color="error">
+                    Log Out
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Navbar.Content>
+            <Navbar.Collapse>
+              {collapseItems.map((item, index) => (
+                <Navbar.CollapseItem
+                  key={item}
+                  activeColor="secondary"
+                  css={{
+                    color: index === collapseItems.length - 1 ? "$error" : "",
+                  }}
+                  isActive={index === 2}
+                >
+                  <Link
+                    color="inherit"
+                    css={{
+                      minWidth: "100%",
+                    }}
+                    href="#"
+                  >
+                    {item}
+                  </Link>
+                </Navbar.CollapseItem>
+              ))}
+            </Navbar.Collapse>
+          </Navbar>
+        </Layout>
     </>
   )
 }
