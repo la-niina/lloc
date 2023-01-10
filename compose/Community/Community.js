46608c93903dceeaad8d68202658f83d6441cd2f
la niina <la.niina.me@gmail.com>
2023-01-11 00:03:37 +0300

    int

--- compose/Community/Community.js
+++ compose/Community/Community.js
@@ -1,7 +1,8 @@
-import { Container, Card, Row, Collapse, Text, Spacer, Image, Grid, Button, Loading, Col, Link } from "@nextui-org/react";
+import { Container, Card, Row, Collapse, Text, Spacer, Image, Grid, Button, Loading, Col, Link, Input } from "@nextui-org/react";
 import { SunIcon } from './icons/SunIcon';
 import { MoonIcon } from './icons/MoonIcon';
 import { AnchorIcon } from './icons/AnchorIcon';
+import { SearchIcon } from './icons/SearchIcon';
 import { Box } from "./Box.js";
 
 export const Content = () => (
@@ -10,8 +11,36 @@ export const Content = () => (
     <Container>
       <Grid xs={12} sm={12} md={12} justify="flex-end" alignContent="center">
         <Grid.Container gap={2} justify="flex-start">
+          <Input
+            clearable
+            contentLeft={
+              <SearchIcon fill="var(--nextui-colors-accents6)" size={16} />
+            }
+            contentLeftStyling={false}
+            css={{
+              w: "100%",
+              "@xsMax": {
+                mw: "300px",
+              },
+              "& .nextui-input-content--left": {
+                h: "100%",
+                ml: "$4",
+                dflex: "center",
+              },
+            }}
+            size="lg"
+            placeholder="Search..."
+            list="form-control"
+          />
+        </Grid.Container>
+      </Grid>
+    </Container>
+
+    <Container>
+      <Grid xs={12} sm={12} md={12} justify="flex-end" alignContent="center">
+        <Grid.Container gap={2} justify="flex-start">
           <Grid xs={4} sm={1.5}>
-            <Card variant="flat"
+            <Card
               isHoverable
               isPressable>
               <Card.Body css={{ p: 0 }}>
@@ -43,7 +72,7 @@ export const Content = () => (
           </Grid>
 
           <Grid xs={4} sm={1.5}>
-            <Card variant="flat"
+            <Card
               isHoverable
               isPressable>
               <Card.Body css={{ p: 0 }}>
@@ -74,7 +103,7 @@ export const Content = () => (
           </Grid>
 
           <Grid xs={4} sm={1.5}>
-            <Card variant="flat"
+            <Card
               isHoverable
               isPressable>
               <Card.Body css={{ p: 0 }}>
@@ -105,7 +134,7 @@ export const Content = () => (
           </Grid>
 
           <Grid xs={4} sm={1.5}>
-            <Card variant="flat"
+            <Card
               isHoverable
               isPressable>
               <Card.Body css={{ p: 0 }}>
@@ -136,7 +165,7 @@ export const Content = () => (
           </Grid>
 
           <Grid xs={4} sm={1.5}>
-            <Card variant="flat"
+            <Card
               isHoverable
               isPressable>
               <Card.Body css={{ p: 0 }}>
@@ -167,38 +196,7 @@ export const Content = () => (
           </Grid>
 
           <Grid xs={4} sm={1.5}>
-            <Card variant="flat"
-              isHoverable
-              isPressable>
-              <Card.Body css={{ p: 0 }}>
-                <Card.Image
-                  src="/folder.png"
-                  objectFit="cover"
-                  width="80%"
-                  height="100%"
-                  alt="folder"
-                />
-              </Card.Body>
-              <Card.Footer isBlurred
-                css={{
-                  justifyItems: "flex-start",
-                  bgBlur: "#0f111466",
-                  borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
-                  bottom: 0,
-                  zIndex: 1,
-                }}>
-                <Row wrap="wrap" justify="space-between" align="center">
-                  <Text size={12} b>New folder</Text>
-                  <Text size={8} css={{ color: "$secondary", fontWeight: "$semibold", fontSize: "$sm" }}>
-                    12.56 MB
-                  </Text>
-                </Row>
-              </Card.Footer>
-            </Card>
-          </Grid>
-          
-          <Grid xs={4} sm={1.5}>
-            <Card variant="flat"
+            <Card
               isHoverable
               isPressable>
               <Card.Body css={{ p: 0 }}>
@@ -228,8 +226,39 @@ export const Content = () => (
             </Card>
           </Grid>
 
-          <Grid xs={6} sm={1.5}>
-            <Card variant="flat"
+          <Grid xs={4} sm={1.5}>
+            <Card
+              isHoverable
+              isPressable>
+              <Card.Body css={{ p: 0 }}>
+                <Card.Image
+                  src="/folder.png"
+                  objectFit="cover"
+                  width="80%"
+                  height="100%"
+                  alt="folder"
+                />
+              </Card.Body>
+              <Card.Footer isBlurred
+                css={{
+                  justifyItems: "flex-start",
+                  bgBlur: "#0f111466",
+                  borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+                  bottom: 0,
+                  zIndex: 1,
+                }}>
+                <Row wrap="wrap" justify="space-between" align="center">
+                  <Text size={12} b>New folder</Text>
+                  <Text size={8} css={{ color: "$secondary", fontWeight: "$semibold", fontSize: "$sm" }}>
+                    12.56 MB
+                  </Text>
+                </Row>
+              </Card.Footer>
+            </Card>
+          </Grid>
+
+          <Grid xs={4} sm={1.5}>
+            <Card
               isHoverable
               isPressable>
               <Card.Body css={{ p: 0 }}>
@@ -260,8 +289,8 @@ export const Content = () => (
             </Card>
           </Grid>
 
-          <Grid xs={6} sm={1.5}>
-            <Card variant="flat"
+          <Grid xs={4} sm={1.5}>
+            <Card
               isHoverable
               isPressable>
               <Card.Body css={{ p: 0 }}>
@@ -292,8 +321,8 @@ export const Content = () => (
             </Card>
           </Grid>
 
-          <Grid xs={6} sm={1.5}>
-            <Card variant="flat"
+          <Grid xs={4} sm={1.5}>
+            <Card
               isHoverable
               isPressable>
               <Card.Body css={{ p: 0 }}>
