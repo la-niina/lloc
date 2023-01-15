a3a06fc9c1d90fac3807fd719407475cb40883c8
la niina <la.niina.me@gmail.com>
2023-01-16 01:41:40 +0300

    int

---
+++ pages/api/app.js
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    status: true,
+    data: {
+      userID: "1",
+      userToken: "18dfade1-189e-41eb-9467-81eec6ed970a",
+      userName: "Adel Ayman",
+      email: "adelayman0000@gmail.com",
+    },
+    message: "Login done successfully",
+  });
+}
