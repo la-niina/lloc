6c6733f26527bc609eea3b54051e58cf46c6cf52
la niina <la.niina.me@gmail.com>
2023-01-26 12:44:26 +0300

    int

--- pages/api/app.js
+++ pages/api/app.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   res.setHeader('Cache-Control', 's-maxage=86400');
   res.status(200).json({
-      "versionCode":1,
+      "versionCode":2,
       "versionName":"1.1.0",
       "content":"1.add something#2.add something",//use # to wrap
       "minSupport":1,	//min support version. while your app versionCode less than  minSupport,You must update app
