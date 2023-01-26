918f425d3b9bdea9a5b37bedf9f1d82eefe2cc55
la niina <la.niina.me@gmail.com>
2023-01-26 12:41:38 +0300

    int

--- pages/api/app.js
+++ pages/api/app.js
@@ -1,13 +1,10 @@
 export default function handler(req, res) {
   res.setHeader('Cache-Control', 's-maxage=86400');
   res.status(200).json({
-    status: true,
-    data: {
-      userID: "1",
-      userToken: "18dfade1-189e-41eb-9467-81eec6ed970a",
-      userName: "Adel Ayman",
-      email: "adelayman0000@gmail.com",
-    },
-    message: "Login done successfully",
+      "versionCode":1,
+      "versionName":"1.1.0",
+      "content":"1.add something#2.add something",//use # to wrap
+      "minSupport":1,	//min support version. while your app versionCode less than  minSupport,You must update app
+      "url":"apk download url"
   });
 }
