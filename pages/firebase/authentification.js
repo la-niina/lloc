eca133bd5b0a6109f274242ef85629fad199d7d9
la niina <la.niina.me@gmail.com>
2023-01-28 00:20:50 +0300

    int

--- pages/firebase/authentification.js
+++ pages/firebase/authentification.js
@@ -6,7 +6,15 @@ import {
 } from "firebase/auth";
 import firebase from "./firebase";
 export const auth = getAuth(firebase);
-const emailpasswordAuth = () => signInWithEmailAndPassword(auth , email, password);
+const emailpasswordAuth = () =>
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
 const logOut = () => signOut(auth);
 
-export{ emailpasswordAuth, logOut }
+export { emailpasswordAuth, logOut };
