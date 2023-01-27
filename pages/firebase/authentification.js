26d6b4676b82cdcc6d4145f906e32a659581e872
la niina <la.niina.me@gmail.com>
2023-01-28 00:18:38 +0300

    int

--- pages/firebase/authentification.js
+++ pages/firebase/authentification.js
@@ -1,15 +1,12 @@
 import {
   getAuth,
-  GoogleAuthProvider,
   signInWithEmailAndPassword,
   onAuthStateChanged,
-  signInAnonymously,
-  signInWithPopup,
   signOut,
 } from "firebase/auth";
 import firebase from "./firebase";
 export const auth = getAuth(firebase);
-export const emailpasswordAuth = () => signInEmailAndPassword(auth , email, password);
-export const signInWithGoogle = () => signInWithPopup(auth, new GoogleAuthProvider());
-export const signInAnonymous = () => signInAnonymously(auth);
-export const logOut = () => signOut(auth);
+const emailpasswordAuth = () => signInWithEmailAndPassword(auth , email, password);
+const logOut = () => signOut(auth);
+
+export{ emailpasswordAuth, logOut }
